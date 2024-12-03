26e8b4cd798d91ec76d3dc6aeb03b0189f677f47
Szőllősy István <szollosyi@kkszki.hu>
2024-12-03 20:48:05 +0100

    Házi 12.03

--- src/DartsList.jsx
+++ src/DartsList.jsx
@@ -1,47 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 export const DartsList = () => {
-    const [darts, setDarts] = useState([]);
-    const [isPending, setPending] = useState(false);
+    const [dartsers, setDarts] = useState([]);
+    const [isFetchPending, setFetchPending] = useState(false);
 
-    useEffect(() => { // fetch-api átírása axios-ra!
-        setPending(true);
-        axios.get('https://darts.sulla.hu/darts')
-        .then(valasz => setDarts(valasz.data))
-        .catch(hiba => console.log(hiba))
-        .finally(() => setPending(false));
+    useEffect(() => {
+        setFetchPending(true);
+        fetch('https://darts.sulla.hu/darts')
+         .then((valasz) => valasz.json())
+         .then((dartsosok) => setDarts(dartsosok))
+         .catch((hiba) => console.log(hiba))
+         .finally(() => setFetchPending(false));
     }, []);
 
     return (
         <div className="container mt-5">
-            <h2 className="text-center">Darts játékosok</h2>
-        {isPending ? (<div className="spinner-border"></div>) : (
-            <div className="row row-cols-1 row-cols-md-3 g-4">
-                
-                {darts.map((dart, index) => (
-                    <div className="col" key={index}>
-                    <div className="card h-100">
-                    <p className="text-dark text-center">Darts játékos neve: {dart.name}</p>
-                    <p className="text-dark text-center">Születési éve: {dart.birth_date}</p>
-                    <p className="text-dark text-center">Megnyert világbajnokságai: {dart.world_ch_won}</p>
-                    <div className="card-body d-flex flex-column align-items-center">
-                        <Link to={dart.profile_url} className="fs-5" target="_blank">Profil link</Link><br />
-                        <Link key={dart.id} to={"/darts/" + dart.id}><img src={dart.image_url ? dart.image_url : 
-                            "https://via.placeholder.com/400x800"} alt={dart.name} 
-                            className="img-fluid" style={{width: "200px"}}/></Link>
-                       </div>
-                       <div className="text-center">
-                       <Link to={"/darts/" + dart.id}><i className="bi bi-text-paragraph fs-3"></i></Link>&nbsp;&nbsp;&nbsp;
-                        <Link to={"/mod-darts/" + dart.id}><i className="bi bi-pencil-square fs-3"></i></Link>&nbsp;&nbsp;&nbsp;
-                        <Link to={"/del-darts/"+ dart.id}><i className="bi bi-trash3 fs-3"></i></Link>
+            <h2 className="text-center">Dartsozók</h2>
+            {isFetchPending ? (
+                <div className="spinner-border"></div>
+            ) : (
+                <div className="row row-cols-1 row-cols-md-3 g-2">
+                    {dartsers.map((darts, index)=> (
+                        <div className="col" key={index}>
+                            <div className="card h-100">
+                            <div className="text-dark text-center"><b>Dartsozó neve:<br /> {darts.name}</b></div>
+                            <div className="text-danger text-center">Születési éve: {darts.birth_date}</div>
+                            <div className="text-danger text-center">Megnyert világbajnokságai: {darts.world_ch_won}</div>
+                            <div className="card-body d-flex flex-column align-items-center">
+                                
+                                <Link to={darts.profile_url} className="fs-6  btn btn-success" target="_blank">Profil link</Link><br/>
+                               <Link key="x" to={"/darts/" + darts.id}>
+                               <img src={darts.image_url ? darts.image_url : "https://via.placeholder.com/400x800"} 
+                               alt={darts.name} className="img-fluid" style={{width: "200px"}} />
+                               </Link><br/>
+                            </div>
+                            <div className="text-center">
+                            <Link to={"/darts/" + darts.id}><i className="bi bi-text-paragraph fs-6 btn btn-primary"></i></Link>&nbsp;&nbsp;&nbsp;
+                            <Link to={"/darts-mod/" + darts.id}><i className="bi bi-pencil-square fs-6 btn btn-warning"></i></Link>&nbsp;&nbsp;&nbsp;
+                            <Link to={"/darts-del/" + darts.id}><i className="bi bi-trash3 fs-6 btn btn-danger"></i></Link><br /><br />
+                            </div>
                         </div>
+                        </div>
+                    ))}
                     </div>
-                    </div>
-                ))}
-            </div>
-        )}
+                )
+            }
         </div>
     );
-};
+}
--- src/DartsSingle.jsx
+++ src/DartsSingle.jsx
@@ -3,50 +3,52 @@ import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 
 export const DartsSingle = () => {
-    const { dartsId } = useParams();
+    const params = useParams();
+    const id = params.dartsId;
     const [darts, setDarts] = useState([]);
     const [isPending, setPending] = useState(false);
 
-    useEffect(() => {   // async-await-külön változóval meghívva!
-        const fetchData = async() => {
-            setPending(true);
-            try {
-                const valasz = await axios.get(`https://darts.sulla.hu/darts/${dartsId}`)
-                setDarts(valasz.data);
-            }
-            catch (hiba) {
-                console.log(hiba);
-            }
-            finally {
-                setPending(false);
-            };    
-    };
-    fetchData(); // itt a useEffect-ben meg kell hívni a függvényt!
-    }, [dartsId]);
-
-    return (
-        <div className="container mt-5">
-            <h2 className="text-center">Darts játékos</h2>
-        {isPending || !dartsId  ? (<div className="spinner-border"></div>) : (
-            <div className="row row-cols-2 justify-content-center align-items-center">
-                    <div className="col">
-                    <div className="card h-250">
-                    <h3 className="text-dark text-center">Darts játékos neve: {darts.name}</h3>
-                    <h4 className="text-dark text-center">Születési éve: {darts.birth_date}</h4>
-                    <h4 className="text-dark text-center">Megnyert világbajnokságai: {darts.world_ch_won}</h4>
-                    <div className="card-body d-flex flex-column align-items-center">
-                        <Link to={darts.profile_url} className="fs-5" target="_blank">Profil link</Link><br />
-                        <img src={darts.image_url ? darts.image_url : 
-                            "https://via.placeholder.com/400x800"} alt={darts.name} 
-                            className="img-fluid" style={{width: "200px"}}/>
-                       </div>
+    useEffect(() => {
+        const fetchData = async () => {
+        setPending(true);
+        try {
+            const response = await axios.get('https://darts.sulla.hu/darts/' + id);
+            setDarts(response.data);
+        }
+        catch (error) {
+            console.log(error);
+        }
+        finally {
+            setPending(false);
+        }
+        };
+        fetchData();
+    }, [id]);
+return (
+    <div className="p-1 m-auto text-center content bg-lavender">
+        {isPending ? (
+            <div className="spinner-border"></div>
+        ) : (
+            <div>
+                <h2>Dartsozók</h2>
+                    <div className="card col-sm3 d-inline-block m-1 p-2">
+                        <p className="text-dark text-center fs-5"><b>Dartsozó neve:<br />{darts.name}</b></p>
+                        <p className="text-danger">Születési éve: {darts.birth_date}</p>
+                        <p className="text-danger">Megnyert világbajnokságai: {darts.world_ch_won}</p>
+                        <div className="card-body">
+                            
+                            <Link to={darts.profile_url} className="btn btn-success fs-6" target="_blank">Profil link</Link><br/><br/>
+                           <Link key="x" to={"/darts/" + darts.id}>
+                           <img src={darts.image_url ? darts.image_url : "https://via.placeholder.com/400x800"} 
+                           alt={darts.name} className="img fluid" style={{width: "200px"}} />
+                           </Link><br/><br/>
+                           <Link to="/"><i className="bi bi-backspace btn btn-primary fs-5"> Vissza</i></Link> &nbsp;&nbsp;
+                           <Link to={"/darts-mod/" + darts.id}><i className="bi bi-pencil-square fs-5 btn btn-warning"> Módosítás</i></Link>
+                        </div>
                     </div>
-                    <div className="d-flex flex-column flex-md-row justify-content-center align-items-center">
-                        <Link to="/"><i className="bi bi-backspace-fill fs-3"></i></Link>&nbsp;&nbsp;&nbsp;
-                        <Link to={"/mod-darts/" + dartsId}><i className="bi bi-pencil-square fs-3"></i></Link></div>
-                    </div>
-            </div>
-        )}
-        </div>
-    );
+                </div>
+            )
+        }
+    </div>
+);
 }
